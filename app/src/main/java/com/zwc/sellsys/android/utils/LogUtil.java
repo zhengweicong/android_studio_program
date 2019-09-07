f791c8bd5bfea9b9da0a652e856e6ff0a3ed6aed
zhengweicong <zhengweicong@qq.com>
2019-09-07 16:55:21 +0800

    update LogUtil

--- app/src/main/java/com/zwc/sellsys/android/utils/LogUtil.java
+++ app/src/main/java/com/zwc/sellsys/android/utils/LogUtil.java
@@ -7,33 +7,53 @@ public class LogUtil {
     private static final String TAG = LogUtil.class.getSimpleName();
     private static boolean debug = true;
 
-    public static void i(String tag,String msg) {
+    public static void i(String tag, String msg) {
         if (debug) {
-            Log.i(tag, msg);
+            if (StringUtil.isEmpty(tag)) {
+                Log.i(TAG, msg);
+            } else {
+                Log.i(tag, msg);
+            }
         }
     }
 
-    public static void d(String tag,String msg) {
+    public static void d(String tag, String msg) {
         if (debug) {
-            Log.d(tag, msg);
+            if (StringUtil.isEmpty(tag)) {
+                Log.i(TAG, msg);
+            } else {
+                Log.d(tag, msg);
+            }
         }
     }
 
-    public static void e(String tag,String msg) {
+    public static void e(String tag, String msg) {
         if (debug) {
-            Log.e(tag, msg);
+            if (StringUtil.isEmpty(tag)) {
+                Log.i(TAG, msg);
+            } else {
+                Log.e(tag, msg);
+            }
         }
     }
 
-    public static void w(String tag,String msg) {
+    public static void w(String tag, String msg) {
         if (debug) {
-            Log.w(tag, msg);
+            if (StringUtil.isEmpty(tag)) {
+                Log.i(TAG, msg);
+            } else {
+                Log.w(tag, msg);
+            }
         }
     }
 
-    public static void v(String tag,String msg) {
+    public static void v(String tag, String msg) {
         if (debug) {
-            Log.v(tag, msg);
+            if (StringUtil.isEmpty(tag)) {
+                Log.i(TAG, msg);
+            } else {
+                Log.v(tag, msg);
+            }
         }
     }
 
